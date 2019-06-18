0601c6fc48c2291b94a12910de5a12e1afd38026
totti <r.funk.7188@gmail.com>
2019-06-18 15:14:25 +0900

    add controller, register, login, logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // login処理の返却
+    public function authenticated(Request $request, $user)
+    {
+        return $user;
+    }
+
+    // logout処理の返却
+    public function loggedOut(Requset $request)
+    {
+        $request->session()->regenerate();
+        return response()->json();
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -38,6 +39,12 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    // register処理の返却
+    public function registered(Request $request, $user)
+    {
+        return $user;
     }
 
     /**
